dbde873692b1842e0a27aff8a5b6c69fe4e5be72
thowes <thw@iki.fi>
2016-08-01 17:55:29 +0300

    Add first redirects

--- server.rb
+++ server.rb
@@ -5,3 +5,33 @@ set :port, 31337
 get '/' do
   erb :index
 end
+
+get '/520' do
+	@title = '520'
+	erb :redir
+end
+
+get '/b1' do
+	@title = 'B1'
+  erb :redir
+end
+
+get '/e330' do
+	@title = 'E330'
+  erb :redir
+end
+
+get '/rc8' do
+	@title = 'RC8'
+  erb :redir
+end
+
+get '/sth' do
+	@title = 'STH'
+  erb :redir
+end
+
+get '/x301' do
+	@title = 'X301'
+  erb :redir
+end
